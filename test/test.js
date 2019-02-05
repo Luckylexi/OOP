095a181b736ca27f6c85765824a7fda251e7b64a
Alexis Hessel <ahessel1998@gmail.com>
2019-02-05 13:33:37 -0700

    added static keyword

--- test/test.js
+++ test/test.js
@@ -1,8 +1,24 @@
+"use strict";
+
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
+var chai = require('chai');
+var marker = require('../marker');
+
+var expect = chai.expect;
+
+describe('Marker', function() {
+  describe('#constructor()', function() {
+    it('should be black when started black', function(){
+        let blackMarker = new marker.Marker("black");
+        assert.equal("black",blackMarker.color);
+    });
+    it('should be initially capped', function() {
+        let blackMarker = new marker.Marker("black");
+        assert.equal(true,blackMarker.capped);        
+    });
+    it('should fail to draw when capped', function() {
+        let blackMarker = new marker.Marker("black");
+        expect(blackMarker.draw().to.throw(Marker.BAD_MARKER_CAPPED_DRAW));
     });
   });
 });
