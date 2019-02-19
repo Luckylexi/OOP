fcd574526a9672ccd373c5820434b90cb14932b4
Alexis Hessel <ahessel1998@gmail.com>
2019-02-19 14:46:07 -0700

    fixed exports and cleaned up

--- js/src/building.js
+++ js/src/building.js
@@ -1,39 +1,50 @@
-class building_error_state{
-    constructor(_problem){
-        this.problem = _problem;
-    }
-}
+
 class building{
-    constructor(_name, _floors, _roomspfloor){
-        console.log(_name + " is being built");
-        this.name = _name;
-        this.floors = _floors;
-        this.roomspfloor = _roomspfloor;
-        this.windowsproom = 4;
-        this.doorslocked = true;
-    }
     
-    //more of a understood idea
-    unlockDoors(){ doorslocked = false;}
-    lockDoors(){ doorslocked = true;}
+    constructor(name, floors, roomspfloor){
+        console.log(name + " is being built");
+        this._name = name;
+        this._floors = floors;
+        this._roomspfloor = roomspfloor;
+        this._windowsproom = 4;
+        this._doorslocked = true;
+    }
 
-    getinbuilding(){
-        if(!this.doorslocked){
-            console.log("You can enter " + name);
+    enterbuilding(){
+        if(!this._doorslocked){
+            console.log("Entering " + this._name);
         }
-        else{
+        else
             throw building.Locked_Building_State;
-        }
-     } 
+    }
 
-    totalRooms() { return roomspfloor * floors;}
-    totalWindows(){ return totalRooms() * windowsproom;}
+    totalrooms(){
+        return this._floors * this._roomspfloor;
+    }
 
-    get name(){ return this.name;}
-    get floors(){ return this.floors;}
+    totalwindows(){
+        return (this.totalrooms() * this._windowsproom);
+    }
+
+    lockDoors(){
+        this._doorslocked = true;
+    }
+    unlockDoors(){
+        this._doorslocked = false;
+    }
+    get Name(){
+        return this._name;
+    }
 }
 
-building.Locked_Building_State = new building_error_state("The building is locked, try again later");
+class errorstate{
+    constructor(problem){
+        this._problem = problem;
+    }
+}
 
-exports.building = Building;
-exports.building_error_state = BuildingErrorState;
+
+building.Locked_Building_State = new errorstate("The building is locked, try again later");
+
+module.exports.building = building ;
+module.exports.errorstate = errorstate;
